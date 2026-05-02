8bc9825581968f55a1592154afe4d80f2b9e41e1
kevinten <596823919@qq.com>
2026-05-02 12:47:13 +0800

    test: improve workflow-id-generator tests

--- src/utils/workflow-id-generator.test.ts
+++ src/utils/workflow-id-generator.test.ts
@@ -2,16 +2,12 @@ import { generateWorkflowId } from './workflow-id-generator';
 
 describe('workflow-id-generator', () => {
   describe('generateWorkflowId', () => {
-    test('should generate unique workflow ID with name and timestamp', () => {
-      const id1 = generateWorkflowId('test-workflow');
-      const id2 = generateWorkflowId('test-workflow');
-      
-      // Should be different due to timestamp
-      expect(id1).not.toBe(id2);
+    test('should generate workflow ID with name and timestamp', () => {
+      const id = generateWorkflowId('test-workflow');
       
       // Should follow format: cleanName-timestamp
-      expect(id1).toMatch(/^testworkflow-[a-z0-9]+$/);
-      expect(id2).toMatch(/^testworkflow-[a-z0-9]+$/);
+      expect(id).toMatch(/^testworkflow-[a-z0-9]+$/);
+      expect(id.length).toBeGreaterThan(12); // 8 chars + hyphen + timestamp
     });
 
     test('should clean and limit name to 8 characters', () => {
@@ -20,14 +16,16 @@ describe('workflow-id-generator', () => {
       
       // Should be limited to 8 characters after cleaning
       expect(id).toMatch(/^thisisav-[a-z0-9]+$/);
+      expect(id.split('-')[0]).toBe('thisisav');
     });
 
     test('should remove special characters from name', () => {
       const nameWithSpecialChars = 'workflow_$test#@';
       const id = generateWorkflowId(nameWithSpecialChars);
       
-      // Should only contain alphanumeric characters
+      // Should only contain alphanumeric characters in the name part
       expect(id).toMatch(/^workflowtest-[a-z0-9]+$/);
+      expect(id.split('-')[0]).toBe('workflowtest');
     });
 
     test('should handle empty string name', () => {
@@ -45,6 +43,24 @@ describe('workflow-id-generator', () => {
       expect(id1).not.toBe(id2);
       expect(id1).toMatch(/^workflow1-[a-z0-9]+$/);
       expect(id2).toMatch(/^workflow2-[a-z0-9]+$/);
+      expect(id1.split('-')[0]).toBe('workflow1');
+      expect(id2.split('-')[0]).toBe('workflow2');
+    });
+
+    test('should handle names shorter than 8 characters', () => {
+      const shortName = 'short';
+      const id = generateWorkflowId(shortName);
+      
+      expect(id).toMatch(/^short-[a-z0-9]+$/);
+      expect(id.split('-')[0]).toBe('short');
+    });
+
+    test('should handle names with numbers', () => {
+      const nameWithNumbers = 'workflow123';
+      const id = generateWorkflowId(nameWithNumbers);
+      
+      expect(id).toMatch(/^workflow123-[a-z0-9]+$/);
+      expect(id.split('-')[0]).toBe('workflow123');
     });
   });
 });
